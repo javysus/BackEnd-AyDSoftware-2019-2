a0eb3c9fd03c276effeef3283636977a8e6f810f
Javiera Villarroel <jvillarroeltoloza@gmail.com>
2020-03-13 18:51:32 -0300

    Detalle

--- src/main/java/com/ayudantia/demo/Autentificacion/Modelos/MyUserDetails.java
+++ src/main/java/com/ayudantia/demo/Autentificacion/Modelos/MyUserDetails.java
@@ -1,20 +1,14 @@
 package com.ayudantia.demo.Autentificacion.Modelos;
 
-import java.util.Arrays;
-import java.util.Collection;
-
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class MyUserDetails implements UserDetails {
 
     private String userName;
     private String password;
-    private List<GrantedAuthority> roles;
 
     public MyUserDetails(User user){
         this.userName = user.getUserName();
@@ -31,31 +25,6 @@ public class MyUserDetails implements UserDetails {
     public String getUsername() {
         // TODO Auto-generated method stub
         return this.userName;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        // TODO Auto-generated method stub
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        // TODO Auto-generated method stub
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        // TODO Auto-generated method stub
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        // TODO Auto-generated method stub
-        // return this.active;
-        return true;
     }
     
 }
