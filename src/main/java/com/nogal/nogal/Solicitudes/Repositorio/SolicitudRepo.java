83d73e9229d69ba7ced4ce8453ac61a2490f6866
Javiera Villarroel <jvillarroeltoloza@gmail.com>
2020-03-21 02:50:13 -0300

    Detalle

--- src/main/java/com/nogal/nogal/Solicitudes/Repositorio/SolicitudRepo.java
+++ src/main/java/com/nogal/nogal/Solicitudes/Repositorio/SolicitudRepo.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface SolicitudRepo extends JpaRepository<Solicitud, Serializable>{
 
-    Solicitud findByID(long ID);
+    public abstract Solicitud findByID(long ID);
     public abstract List<Solicitud> findByTecnicoOrCliente(long tecnico, long cliente);
 
 }
